c4adabb1a7c669b98a5e35677fcbcb90fb890ed0
Pedro Silva <mrgpedrosilva@gmail.com>
2024-11-12 20:47:36 -0300

    Init

---
+++ src/main/java/com/pedro/vacination/VacinationApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.vacination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/vacination/VacinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.vacination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
